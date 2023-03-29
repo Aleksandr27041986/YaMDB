4fb2fc765ed5da52d42fe2996163f91670549955
Дмитрий Кучмин <kuchminog@gmail.com>
2023-03-30 01:33:50 +0300

    Создана панель администратора  User

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(UserAdmin):
+    
+    list_display = ['email', 'username', 'role', 'is_active']
+    empty_value_display = '-пусто-'
+
+admin.site.register(User, UserAdmin)
